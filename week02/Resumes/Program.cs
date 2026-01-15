34c1b1fb4e41ccf10d723f169a779c1a6becad7c
Osasere Ikponmwosa <edenosas1@gmail.com>
2026-01-15 16:49:27 +0100

    Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        Resume myResume = new Resume();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2030;
+
+        job2._jobTitle = "Product Manager";
+        job2._company = "Apple";
+        job2._startYear = 2000;
+        job2._endYear = 2040;
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._name = "Allison Rose";
+        myResume.Display();
+   
     }
-}
+} 
